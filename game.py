b949018c73c02a1c58c957f3a5433872ee08a1ba
kosyakarseniy20080 <144513307+kosyakarseniy20080@users.noreply.github.com>
2024-02-03 09:43:43 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -3,8 +3,8 @@ import pygame
 import random
 
 # переменные с размерами экрана
-WIDTH = 360
-HEIGHT = 480
+WIDTH = 1280
+HEIGHT = 720
 FPS = 60
 
 WHITE = (255, 255, 255)
@@ -39,15 +39,27 @@ colorApple = (255, 150, 100)
 x_apple = 100
 y_apple = 200
 
+ing = pygame.image.load("./apple.jpg")
+scale = pygame.transform.scale(ing, (ing.get_width()//4,ing.get_height()//4))
+scale_rect = scale.get_rect(center=(WIDTH//2,HEIGHT//2))
 
 
+# ХК Спартак Москва
+spr_ing = pygame.image.load("./spartak.png")
+scale_spr = pygame.transform.scale(spr_ing, (200,200))
+spr_rect = scale_spr.get_rect()
+spr_rect.center = 100, 100
 
+
+# pygame.time.wait(5000)
 
 while running:
     # Держим цикл на правильной скорости 
     clock.tick(FPS)
     # заполнить экран цветом
     screen.fill(screenColor)
+    screen.blit(scale,scale_rect)
+    screen.blit(scale_spr,spr_rect)
 
     # рисуем прямоугольник в screen c цветом (100,50,200) с координатами и размером
     pygame.draw.rect(screen,(100,50,200),[x,y,width_player,height_player] )
@@ -56,11 +68,14 @@ while running:
     if (x<0 or x > WIDTH - 50):
         running = False
         pass
-    
+    # Столкновение с шариком
     if ((x < x_apple+15  and x < x_apple-15 < x + width_player) and (y < y_apple+15 and y < y_apple-15 < y + height_player)):
         width_player += 10
         x_apple = random.randint(0,WIDTH)
         y_apple = random.randint(0,HEIGHT)
+
+        scale_rect.center = x_apple, y_apple
+
         pygame.draw.circle(screen,colorApple,[x_apple,y_apple],15)
 
     keys = pygame.key.get_pressed()
@@ -80,5 +95,5 @@ while running:
             running = False      
     
 
-    pygame.display.flip()
+    pygame.display.update()
 pygame.quit()
