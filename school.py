74c64b1185b1fe80cbb879e95286bac3c31b65ed
kosyakarseniy20080 <144513307+kosyakarseniy20080@users.noreply.github.com>
2024-02-11 14:48:35 +1000

    Create school.py

---
+++ school.py
@@ -0,0 +1,17 @@
+def copy_documents(n, m):
+
+ if n < 0 or m < 0:
+
+  return 0
+
+ else:
+
+  return n * m
+
+
+
+# Примеры использования:
+
+print(copy_documents(5, 6)) # Вывод: 30
+
+print(copy_documents(-5, 10)) # Вывод: 0
